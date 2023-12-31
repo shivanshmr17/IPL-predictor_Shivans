9e10120d5ca2955e4a46bf4c94c661e037589e70
shivanshmr17 <110936447+shivanshmr17@users.noreply.github.com>
2024-01-01 04:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,6 @@ import pickle
 import os
 import pandas as pd
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8501))
-    st.set_option('server.port', port)
-    st.write("Running on port:", port)
-    st.run()
-
-
 teams = ['Sunrisers Hyderabad',
  'Mumbai Indians',
  'Royal Challengers Bangalore',
